6a29988f3a31fa1d8a031235796666bb20bd3938
abdul mannan ullah khan <abdulmannanullahkhan115@gmail.com>
2024-06-05 02:11:54 +0500

    simple cli-calculator

---
+++ index.js
@@ -0,0 +1,41 @@
+import inquirer from 'inquirer';
+const theAnswer = await inquirer.prompt([
+    { name: "firstnumber",
+        type: "number",
+        message: "enter your first number" },
+    { name: "secondnumber",
+        type: "number",
+        message: "enter your second number" },
+    {
+        name: "tools",
+        type: "list",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+        message: "enter the correct tool to perform the calculation"
+    }
+]);
+//apply condiotional mstatements for certain actions
+if (theAnswer.tools === "addition") {
+    [
+        console.log(theAnswer.firstnumber + theAnswer.secondnumber)
+    ];
+}
+else if (theAnswer.tools === "subtraction") {
+    [
+        console.log(theAnswer.firstnumber - theAnswer.secondnumber)
+    ];
+}
+else if (theAnswer === "multiplication") {
+    [
+        console.log(theAnswer.firstnumber * theAnswer.secondnumber)
+    ];
+}
+else if (theAnswer === "division") {
+    [
+        console.log(theAnswer.firstnumber / theAnswer.secondnumber)
+    ];
+}
+else {
+    [
+        console.log("invalid tool")
+    ];
+}
